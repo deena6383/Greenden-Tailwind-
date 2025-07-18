91e777a1c8fe7c0cd4ab42b8aeba76c1d1830107
deena6383 <deena63830507@gmil.com>
2025-07-18 22:18:16 +0530

    update tailwind project

---
+++ files/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./product.html",
+    "./contact.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ], // Adjust path to your files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
